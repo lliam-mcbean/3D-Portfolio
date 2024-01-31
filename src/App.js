61e32919a688b0743e17a6fd76321b6f1dce96c4
soupisthecurrencyofthefuture <lliam.mcbean@gmail.com>
2024-01-31 15:40:40 -0800

    added get request for handling user drawings

--- src/App.js
+++ src/App.js
@@ -8,22 +8,12 @@ axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 function App() {
   const [isLoading, setIsLoading] = useState(true)
-  const [userDrawings, setUserDrawings] = useState([])
-
-  useEffect(() => {
-    axios.get('/drawings').then((res) => {
-      if (res.status === 200) {
-        setUserDrawings(res.data)
-      }
-      console.log('axios response', res)
-    })
-  }, [])
 
   return (
     <div className='w-screen h-screen'>
       <div className="w-screen h-screen absolute">
         <Canvas colorManagement shadows camera={{position: [50, 30, 50], far: 5000}}>
-          <World userDrawings={userDrawings} />
+          <World />
         </Canvas>
       </div>
       {/* {isLoading && <Loader setIsLoading={setIsLoading} />} */}
--- src/components/World/Painting/index.jsx
+++ src/components/World/Painting/index.jsx
@@ -1,10 +1,33 @@
-import { Line } from '@react-three/drei'
-import React, { useState } from 'react'
+import { Line, Html } from '@react-three/drei'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import * as THREE from 'three'
 
 export default function Painting({position, size, rotation}) {
   const [isDrawing, setIsDrawing] = useState(false)
+  const [userDrawings, setUserDrawings] = useState([])
   const [drawing, setDrawing] = useState([])
+
+  useEffect(() => {
+    axios.get('/drawings').then((res) => {
+      if (res.status === 200) {
+        setUserDrawings(res.data)
+      }
+      console.log('axios response', res)
+    })
+  }, [])
+
+  const convertCoords = (coords) => {
+    const returnValue = coords.map((el) => [el.x, el.y, el.z])
+    return returnValue
+  }
+
+  const submitDrawing = () => {
+    const convertedCoords = convertCoords(drawing)
+    axios.post('/drawings', {data: convertedCoords}).then((res) => {
+      console.log('this is the drawing submit', res)
+    })
+  }
 
   return (
     <>
@@ -26,12 +49,24 @@ export default function Painting({position, size, rotation}) {
        <planeGeometry args={size} />
        <meshBasicMaterial transparent opacity={0}/>
       </mesh>
+      <Html position={[position[0] + 30, position[1] - 35, position[2] + 10]} rotation={[rotation[0], (Math.PI / 2) - rotation[1], rotation[2]]}>
+        <div onClick={() => {
+          submitDrawing()
+        }} className='bg-red-500 cursor-pointer'>submit</div>
+      </Html>
       <mesh>
 						<Line
 							points={drawing.length > 0 ? drawing : [new THREE.Vector3(0,0,0)]}
 							color={0x00ff00}
 							lineWidth={1.5}
 						/>
+            {userDrawings.length > 0 &&
+            userDrawings.map((el) => <Line 
+              points={el.map((coord) => new THREE.Vector3(...coord))}
+              color={0x00ff00}
+              lineWidth={1.5}
+            />)
+            }
 			</mesh>
     </>
   )
