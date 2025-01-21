a735f4bac3952b8c4003925d9d6d5355f22c5c76
lliam-mcbean <lliam.mcbean@gmail.com>
2025-01-21 12:29:29 -0800

    change button style

--- src/components/Explore/index.jsx
+++ src/components/Explore/index.jsx
@@ -6,12 +6,10 @@ export default function Explore() {
 
     return (
         <div className='fixed bottom-0 right-0 p-8'>
-            <button onClick={() => setExplore((prev) => !prev)} className="group relative px-6 py-3 text-black">
-                <span className="absolute inset-0 h-full w-full -translate-x-2 -translate-y-2 transform bg-gray-200 transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-                <span className="absolute z-[1] h-full w-full -translate-x-8 -translate-y-5 border-b-2 border-e-2 border-white group-hover:border-none"></span>
-                <span className="absolute inset-0 h-full w-full translate-x-2 translate-y-2 transform border-s-2 border-t-2 border-white bg-gray-200 transition duration-300 ease-out group-hover:translate-x-0 group-hover:translate-y-0"></span>
-                <span className="absolute inset-0 z-[1] h-full w-full border-2 border-black"></span>
-                <span className="relative">{explore ? 'Return To Home' : "Explore My Desk"}</span>
+            <button onClick={() => setExplore((prev) => !prev)} class="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-1 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                {explore ? 'Return To Home' : "Explore My Desk"}
+                </span>
             </button>
         </div>
     )
